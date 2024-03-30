63a05a3a8dea4e3b91459ac3f547886bd34e5727
Aytaditya <adityaaryan531@gmail.com>
2024-03-30 17:11:35 +0530

    learning solidity

---
+++ contract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+contract inbox{
+    string public message;
+
+    function inbox(string initialMessage) public {
+        message=initialMessage;
+
+    }
+    function setMessage(string newMessage) public{
+        message=newMessage;
+    }
+    function getMessage() public view returns (string){
+        return message;
+    }
+}
