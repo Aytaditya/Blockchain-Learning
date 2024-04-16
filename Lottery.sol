d9a33fc9b4d4bc5a4e6148347db35acddc2b745f
Aytaditya <adityaaryan531@gmail.com>
2024-04-16 13:27:05 +0530

    updated lottery code

--- Lottery.sol
+++ Lottery.sol
@@ -12,7 +12,17 @@ contract Lottery {
 
     // whenever we create a function that expects to receieve ether we mark it as payable
     function enter() public payable {
+        require(msg.value >0.01 ether);  //amount of ether that was sent (in wei)
         players.push(msg.sender);
+    }
+
+    //dont want anyone to call this
+    //this is sudo-random
+    function random() private view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+
+    //now is decrepted we now use block.timestamp for time
+    //this random function will return very large number
     }
 
     
